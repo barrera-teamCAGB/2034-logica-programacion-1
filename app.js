9091003cb0c59768c7ccb0827a52d45b2b1417a2
barrera-teamCAGB <156938736+barrera-teamCAGB@users.noreply.github.com>
2024-01-17 15:10:16 -0600

    agregando archivo js

---
+++ app.js
@@ -0,0 +1,5 @@
+alert ('Hello Word');
+// puede usarse comillas simple o comillas dobles
+// al final de cada linea de codigo se recomienda terminar con punto y coma
+// El método Window.alert() muestra un dialogo de alerta con un mensaje opcional, y aguardará hasta que el usuario cierre la ventana de diálogo.
+
