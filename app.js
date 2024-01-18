5e92c659686ebd5f0bf01eaf5807506cef55bccc
barrera-teamCAGB <156938736+barrera-teamCAGB@users.noreply.github.com>
2024-01-18 09:42:02 -0600

    se agrega el metodo prompt

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+
+// El método Window.alert() muestra un dialogo de alerta con un mensaje opcional, y aguardará hasta que el usuario cierre la ventana de diálogo
 alert ('Hello Word');
 // puede usarse comillas simple o comillas dobles
 // al final de cada linea de codigo se recomienda terminar con punto y coma
-// El método Window.alert() muestra un dialogo de alerta con un mensaje opcional, y aguardará hasta que el usuario cierre la ventana de diálogo.
+
+// El método Window.prompt() muestra un diálogo con mensaje opcional, que solicita al usuario que introduzca un texto.
+prompt ("Este es un juego de adivina el numero entre el 1 y 10 para iniciar, Indicame un numero: ");
+
 
