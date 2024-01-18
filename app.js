f20c072b6bbe597dc16b20cf678073e2fa81b1dd
barrera-teamCAGB <156938736+barrera-teamCAGB@users.noreply.github.com>
2024-01-18 10:03:15 -0600

    usando la consola del navegador para desplegar mensajes

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ alert ('Hello Word');
 // al final de cada linea de codigo se recomienda terminar con punto y coma
 
 // El método Window.prompt() muestra un diálogo con mensaje opcional, que solicita al usuario que introduzca un texto.
-prompt ("Este es un juego de adivina el numero entre el 1 y 10 para iniciar, Indicame un numero: ");
+//prompt ("Este es un juego de adivina el numero entre el 1 y 10 para iniciar, Indicame un numero: ");
 
+// Creamos una variable por el momento solo usaremos let, con el noombre en camelCase
+let numeroUsuario = prompt ("Este es un juego de adivina el numero entre el 1 y 10 para iniciar, Indicame un numero: ");
+
+// Console.log() Muestra un mensaje en la consola web (o del intérprete JavaScript).
+console.log (numeroUsuario);
 
