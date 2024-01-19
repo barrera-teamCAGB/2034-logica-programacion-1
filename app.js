ef74a51978b65e0715a86b66d81bd2a261fe9be1
barrera-teamCAGB <156938736+barrera-teamCAGB@users.noreply.github.com>
2024-01-18 21:04:13 -0600

    desafio 5

--- app.js
+++ app.js
@@ -70,3 +70,7 @@ console.log(edad);
 let numeroDeVentas = 50;
 console.log(numeroDeVentas);
 
+// 5 Establece una variable saldoDisponible y asígnale el valor 1000.
+let saldoDisponible = 1000;
+console.log(saldoDisponible);
+
