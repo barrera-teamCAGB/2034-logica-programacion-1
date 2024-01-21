49594625c90e6056af7b9c3639922b1b7df2aa0f
barrera-teamCAGB <156938736+barrera-teamCAGB@users.noreply.github.com>
2024-01-21 11:45:52 -0600

    ejercico 1

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-//Variables
-let numeroSecreto = 2;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+// //Variables
+// let numeroSecreto = 2;
+// let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
-console.log(numeroUsuario);
-/*
-Este código realiza
-la comparación
-*/
-if (numeroUsuario == numeroSecreto) {
-    //Acertamos, fue verdadera la condición
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
+// console.log(numeroUsuario);
+// /*
+// Este código realiza
+// la comparación
+// */
+// if (numeroUsuario == numeroSecreto) {
+//     //Acertamos, fue verdadera la condición
+//     // la comilla invertida (interpolacion) sirve para colocar tanto para combinar valor literal con variables conocido tambien con la concatenacion pero aqui todo va entre las comillas invertidas, las variables se colocan iniciando con el simbolo de peso y la variable va entre corchetes, tambien se puede colocar codigo java pero eso se vera despues 
+//     alert(`Acertaste, el número es: ${numeroUsuario}`);
+// } else {
+//     //La condición no se cumplió
+//     alert('Lo siento, no acertaste el número');
+// }
+
+// ejercio 1 
+let edad = prompt("Ingrese su edad:");
+
+if (edad >= 18) {
+  console.log("Eres mayor de edad.");
 } else {
-    //La condición no se cumplió
-    alert('Lo siento, no acertaste el número');
+  console.log("Eres menor de edad.");
 }
+
