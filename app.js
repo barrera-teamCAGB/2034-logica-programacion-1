ccdef131bdef376b107528d1dd4daa9603b27c58
barrera-teamCAGB <156938736+barrera-teamCAGB@users.noreply.github.com>
2024-01-23 22:10:11 -0600

    probando la opcion A

--- app.js
+++ app.js
@@ -3,12 +3,33 @@ let porcentajeDescuento = 0;
 
 let cantidadMillas = prompt("Ingrese la cantidad de millas");
 
-if (cantidadMillas > 30000) {
+// Personas con una cantidad de millas inferior a 5,000 no reciben descuento.
+
+// Personas con una cantidad de millas superior a 30,000 reciben un 20% de descuento.
+
+// Personas con una cantidad de millas superior a 5,000, pero inferior a 30,000 reciben un 10% de descuento.
+
+/* if (cantidadMillas > 30000) {
     porcentajeDescuento = 20;	
 } else if (cantidadMillas > 5000) {
     porcentajeDescuento = 10;	
 } else {
     porcentajeDescuento = 0;
+} */
+
+
+// probando las opciones
+// opcion A
+if (cantidadMillas > 5000) {
+    porcentajeDescuento = 10;
 }
+
+if (cantidadMillas > 30000) {
+    porcentajeDescuento = 20;	
+} else {
+    porcentajeDescuento = 0;
+}
+
+
 
 console.log(porcentajeDescuento);
