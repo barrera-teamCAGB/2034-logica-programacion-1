b310256a8f0236d65e7bf5c9f61eb6a622145c97
barrera-teamCAGB <156938736+barrera-teamCAGB@users.noreply.github.com>
2024-01-23 23:55:22 -0600

    desafio 5

--- app.js
+++ app.js
@@ -26,14 +26,18 @@ if (numeroUsuario > 0) {
 } */
 
 // 3 Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100, muestra "¡Felicidades, has ganado!". En caso contrario, muestra "Intenta nuevamente para ganar.".
-let puntuacion = prompt ("Cual es tu puntuacion: ");
+/* let puntuacion = prompt ("Cual es tu puntuacion: ");
 
 if (puntuacion >= 100) {
     console.log ("¡Felicidades, has ganado!", puntuacion);
 } else {
     console.log ("Intenta nuevamente para ganar", puntuacion);
-}
+} */
 
 // 4 Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor del saldo.
+//let saldo = 100;
+
 
 // 5 Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.
+let nombre = prompt("Indiquenos su nombre: ");
+alert(`Bienvenido ${nombre}`);
