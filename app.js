b8fafc6b5e1f50aaa203fe3d338099e8042cb678
barrera-teamCAGB <156938736+barrera-teamCAGB@users.noreply.github.com>
2024-01-25 09:41:47 -0600

    propuesta funcionando con untervalo de numero y pidiendo cuantos intentos quiere

--- app.js
+++ app.js
@@ -15,18 +15,47 @@
 //let maximosIntentos = prompt("Cuantos intentos te gustaria tener para adivinar el numero secreto:");
 // como opcion a esta variable le colocaremos el parseFloat() para asegurar que sea un numero
 
-// ahora comentaremos y sustituiremos 
+// ahora comentaremos y sustituiremos
 
-let numeroMaximoPosible = 100;
-let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
+// comentamos esta linea y creamos una nueva variable donde despues se la pediremos al user y sera el primer intervalo
+//let numeroMaximoPosible = 100;
+let intervalo1;
+// creamos el segundo intervalo donde tambien se lo pediremos
+let intervalo2;
+// comentamos la siguiente y solo crearemos la variable
+// let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
+let numeroSecreto;
+// la siguiente l dejamos
 let numeroUsuario = 0;
+// la siguiente la dejamos y por lo menos tendra un intento
 let intentos = 1;
+// la siguiente ya no se utiliza por que se izo con no recuerdo el nombre pero es donde se abrevia el if
 //let palabraVeces = 'vez';
-let maximosIntentos = 6;
+// la comentamos y solo la creamos para despues pedirserla al usuario
+// let maximosIntentos = 6;
+let maximosIntentos;
+
+// iniciamos donde explicamos como se juega
+alert("este es un juego de adivinar el numero, primero se le pedira 2 numeros que definiran el intervalo de los numeros donde aleatoriamente existira un numero incluyendose esos 2 numeros");
+// podemos colocarle la funcion de parsefloat para crear el numero entero ese va antes del prompt lo cual lo incluira sintaxist de como quedaria parseFloat(prompt) pero para efector de operaciones sera el intervalo1 con Math.ceil y el intervalo2 con Math.floor para que el resultado de la operacion este correcto
+intervalo1 = Math.ceil(prompt("primer numero donde iniciara el primer intervalo, este sera incluido en el intervalo"));
+console.log("vemos que el primer numero es: " + intervalo1);
+intervalo2 = Math.floor(prompt("segundo numero donde terminara el intervalo, este sera incluido en el intervalo"));
+console.log("vemos que el segundo numero fue: " + intervalo2);
+
+//creamos la formula para crear el numero secreto o aleatorio * (numero2-numero1+1) + numero1
+numeroSecreto = Math.floor(Math.random()*(intervalo2-intervalo1+1)+intervalo1);
+// verificamos cual fue
+console.log("el numero secreto es: " + numeroSecreto);
+
+// ahora le pedimos en cuantos intentos lo quiere adivinar
+maximosIntentos = prompt("En cuantos intentos te gustaria adivinar el numero secreto:");
+// comprobamos que numero coloco
+console.log("el numero de intervalo fue de: " + intentos);
 
 
 while (numeroUsuario != numeroSecreto) {
-    numeroUsuario = parseInt(prompt(`Me indicas un número entre 1 y ${numeroMaximoPosible}  por favor:`));
+    numeroUsuario = parseInt(prompt(`Me indicas un número entre ${intervalo1} y ${intervalo2}  por favor:`));
 
     console.log(typeof(numeroUsuario));
     if (numeroUsuario == numeroSecreto) {
